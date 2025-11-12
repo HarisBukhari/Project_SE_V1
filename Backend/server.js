6cef4e60208d9023426e3ff8e92d781a42e88e47
Haris Bukhari <syedharishussain86@gmail.com>
2025-11-12 11:17:34 +0300

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -8,6 +8,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//Server check
+app.get('/', (req, res) => {
+  res.send('Running');
+});
+
 // Routes
 app.use('/api/chat', require('./routes/chat'));
 
